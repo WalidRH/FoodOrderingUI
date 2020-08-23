88af2ceb9f7f5323693d9188d4e0a3eba41e7464
WalidRH <rahouwalid17@gmail.com>
2020-08-23 18:48:04 +0100

    fix unkonw element by adding homeComponenet in the feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,19 +2,18 @@ import { IntroBlockComponent } from './blocks/intro-block/intro-block.component'
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRouter } from './router.module';
+import { HomeComponent } from './home.component';
 
 
 
 @NgModule({
   declarations: [
+    HomeComponent,
     IntroBlockComponent
   ],
   imports: [
     CommonModule,
     HomeRouter
-  ],
-  exports: [
-    IntroBlockComponent
   ]
 })
 export class HomeModule { }
