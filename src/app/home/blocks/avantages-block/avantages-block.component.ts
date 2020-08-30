1624f5069e9652d4312e4ab06b06ff50e46a7401
WalidRH <rahouwalid17@gmail.com>
2020-08-30 02:06:04 +0100

    adding advantages block

--- src/app/home/blocks/avantages-block/avantages-block.component.ts
+++ src/app/home/blocks/avantages-block/avantages-block.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvantagesBlockComponent implements OnInit {
 
+  advantagesList = [
+    {
+      'title': 'More than 20,000 dishes to orders',
+      'description': 'we have more than 20,000 delicious dishes that you can choose from.'
+    },
+    {
+      'title': 'Easy Ordering by Phone',
+      'description': 'This allows you to order quickly and easily. Accessible at any time.'
+    },
+    {
+      'title': 'Your table will be waiting for you',
+      'description': 'By the time you arrive to our restaurent, your place will be reserved for you.'
+    },
+    {
+      'title': 'Your meal will be ready',
+      'description': 'By the time you arrive to our restaurent, the meal that you order, is coocked and ready to be served. So no need to wait.'
+    },
+    {
+      'title': '100% positive feedbacks',
+      'description': 'We care about our customers, that is why we get 100% positive feedbacks.'
+    },
+    {
+      'title': 'delicious meals',
+      'description': 'We have the top chefs all arowned the world, and all our dishes got 5 stars in quality'
+    }
+  ];
+
+  listsize = this.advantagesList.length;
   constructor() { }
 
   ngOnInit(): void {
