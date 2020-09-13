fa860d125d1f7bd79e3f89152a57e85190dd04ff
WalidRH <rahouwalid17@gmail.com>
2020-09-13 22:23:01 +0100

    adding authentication manager

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './../../shared/service/authentication.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('authForm') authenticationData: NgForm;
+  e_mail = '';
+  password = '';
+  loading = false;
+  error = false;
+  errorMessage: string;
+  constructor( private authenticationService: AuthenticationService ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log('Authentication Form ', this.authenticationData);
+    this.loading = true;
+    this.authenticationService.login(
+      this.authenticationData.form.value.email,
+      this.authenticationData.form.value.pass
+      ).subscribe(
+      responseData => {
+        this.loading = false;
+        this.error = false;
+        console.log(' responseData ==> ', responseData);
+      },
+      error => {
+        this.loading = false;
+        this.error = true;
+        console.log(' ERROR response ==> ', error.error.message);
+        this.errorMessage = error.error.message;
+      }
+    );
+
+    this.authenticationService.authenticatedUser.subscribe(
+      responseData => {
+        console.log('DATA ', responseData);
+      }
+    );
   }
 
 }
---
+++ src/app/shared/service/authentication-interceptor.service.ts
@@ -0,0 +1,33 @@
+import { AuthenticationService } from './authentication.service';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { take, exhaustMap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationInterceptorService implements HttpInterceptor{
+
+  constructor( private authenticationService: AuthenticationService ) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.authenticationService.authenticatedUser.pipe(
+      take(1),
+      exhaustMap(
+        authenticatedUser => {
+          if (!authenticatedUser){
+            return next.handle(req);
+          }
+          const modifiedRequest = req.clone({
+            headers : new HttpHeaders({
+              Authorization: 'Bearer ' + authenticatedUser.token
+            })
+          });
+          return next.handle(modifiedRequest);
+        }
+      )
+    );
+  }
+
+}
