810b5bc58d5e3d069beaffcd2da286b0fd78d722
WalidRH <rahouwalid17@gmail.com>
2020-10-10 04:49:25 +0100

    adding orde delete feature

--- src/app/cart/blocks/orders-list/orders-list.component.ts
+++ src/app/cart/blocks/orders-list/orders-list.component.ts
@@ -1,3 +1,5 @@
+import { NotificationModule } from './../../../shared/model/notification.module';
+import { SharedUtilsService } from './../../../shared/service/shared-utils.service';
 import { OrderHttpRequestService } from './../../../shared/service/http-services/order-http-request.service';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
@@ -11,7 +13,7 @@ import { Order } from '../../../shared/model/order.module';
 export class OrdersListComponent implements OnInit {
 
   orderItemsArray: Order[];
-  constructor( private orderHttpService: OrderHttpRequestService ) { }
+  constructor( private orderHttpService: OrderHttpRequestService, private utilService: SharedUtilsService ) { }
 
   ngOnInit(): void {
     this.orderHttpService.findOrderItemsToValidate();
@@ -22,6 +24,19 @@ export class OrdersListComponent implements OnInit {
       }
     );
     console.log('MY ORDERS LIST ', this.orderItemsArray);
+  }
+
+  remove(idOrder: string){
+    console.log('DELETING ORDER ID : ' + idOrder );
+    this.orderHttpService.deleteOrder(idOrder).subscribe(
+      response => {
+        this.utilService.notificationMessage.next(new NotificationModule(response.message, 'SUCCESS'));
+        this.orderHttpService.findOrderItemsToValidate();
+      },
+      error => {
+        this.utilService.notificationMessage.next(new NotificationModule(error.error.error, 'FAILED'));
+      }
+    );
   }
 
 }
