ae9d15030def533ad598c2124f83a4c3773666d8
WalidRH <rahouwalid17@gmail.com>
2020-10-11 02:17:31 +0100

    adding list of ordered meals

--- src/app/cart/blocks/orders-list/orders-list.component.ts
+++ src/app/cart/blocks/orders-list/orders-list.component.ts
@@ -1,8 +1,10 @@
+import { element } from 'protractor';
+import { Router } from '@angular/router';
 import { NotificationModule } from './../../../shared/model/notification.module';
 import { SharedUtilsService } from './../../../shared/service/shared-utils.service';
 import { OrderHttpRequestService } from './../../../shared/service/http-services/order-http-request.service';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild, OnChanges, ElementRef } from '@angular/core';
+import { NgForm, NgControl } from '@angular/forms';
 import { Order } from '../../../shared/model/order.module';
 
 @Component({
@@ -10,10 +12,13 @@ import { Order } from '../../../shared/model/order.module';
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.scss']
 })
-export class OrdersListComponent implements OnInit {
+export class OrdersListComponent implements OnInit, OnChanges {
 
   orderItemsArray: Order[];
-  constructor( private orderHttpService: OrderHttpRequestService, private utilService: SharedUtilsService ) { }
+  preBooking: boolean;
+  @ViewChild('arrayOrdersForm') arrayOrdersForm: NgForm;
+  @ViewChild('quantity') quantity: ElementRef;
+  constructor( private orderHttpService: OrderHttpRequestService, private utilService: SharedUtilsService, private route: Router ) { }
 
   ngOnInit(): void {
     this.orderHttpService.findOrderItemsToValidate();
@@ -24,6 +29,11 @@ export class OrdersListComponent implements OnInit {
       }
     );
     console.log('MY ORDERS LIST ', this.orderItemsArray);
+    
+  }
+
+  ngOnChanges(): void{
+    console.log('WAL CHANGES ', this.arrayOrdersForm);
   }
 
   remove(idOrder: string){
@@ -37,6 +47,23 @@ export class OrdersListComponent implements OnInit {
         this.utilService.notificationMessage.next(new NotificationModule(error.error.error, 'FAILED'));
       }
     );
+  }
+
+  onSubmit(){
+    if ( this.orderItemsArray.length === 0 ){
+      this.utilService.notificationMessage.next(new NotificationModule('Nothing to update: PLEASE ORDER SOME MEALS', 'FAILED'));
+      this.route.navigate(['menu']);
+    }else{
+      console.log('ARRAY ORDER ',this.orderItemsArray);
+      this.orderItemsArray.forEach(element => {
+        console.log('CONSOLE : ', this.arrayOrdersForm.form.controls[element.quantity]);
+        console.log('ELEMENT : ', element.quantity);
+        if ( element.quantity !== this.arrayOrdersForm.form.controls[element.quantity].value){
+            console.log('UPDATING ', element.ref);
+        }
+      });
+    }
+
   }
 
 }
