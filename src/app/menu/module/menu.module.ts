f4c7ff65e2770771540f68d75d59f2d5b0a344f6
WalidRH <rahouwalid17@gmail.com>
2020-10-18 01:16:13 +0100

    set " add menu " button

--- src/app/menu/module/menu.module.ts
+++ src/app/menu/module/menu.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FilterBlockComponent } from '../blocks/filter-block/filter-block.component';
 import { MenuComponent } from '../menu.component';
 import { ListDishesComponent } from '../blocks/list-dishes/list-dishes.component';
+import { AddBlockComponent } from '../blocks/add-block/add-block.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
     FilterBlockComponent,
-    ListDishesComponent
+    ListDishesComponent,
+    AddBlockComponent
   ],
   imports: [
     CommonModule,
