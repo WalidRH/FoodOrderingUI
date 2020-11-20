ea63f4da62851743192aeb7b9519fc4176960cf3
WalidRH <rahouwalid17@gmail.com>
2020-11-21 00:15:28 +0100

    changing ordered item template

--- src/app/orders/blocks/ordered-items/ordered-items.component.ts
+++ src/app/orders/blocks/ordered-items/ordered-items.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderedItemsComponent implements OnInit {
 
+  submittedOrders = false;
+  preReservedOrders = false;
+
   constructor() { }
 
   ngOnInit(): void {
