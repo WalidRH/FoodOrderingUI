75e36803db82b2f55de52f76271af5849414ad47
WalidRH <rahouwalid17@gmail.com>
2020-12-31 19:20:18 +0100

    home page - adding parallax effect

--- src/app/home/blocks/intro-block/intro-block.component.ts
+++ src/app/home/blocks/intro-block/intro-block.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-intro-block',
   templateUrl: './intro-block.component.html',
-  styleUrls: ['./intro-block.component.scss']
+  styleUrls: ['./intro-block.component.scss'],
 })
 export class IntroBlockComponent implements OnInit {
 
-  constructor() { }
+  offset: any;
+  isScroll = false;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+    this.offset = window.pageYOffset * 0.5;
+    console.log("SCROLLING : ",this.offset);
+    this.isScroll = true;
   }
-
 }
